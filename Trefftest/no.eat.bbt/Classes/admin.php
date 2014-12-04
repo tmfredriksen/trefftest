65ced29d9d96f1785f2233fbaa9ac1fd6ac0ec0e
eivindjs <eivind.skreddernes@gmail.com>
2014-12-04 15:11:56 +0100

    login siden opp å kjører

--- Trefftest/no.eat.bbt/Classes/admin.php
+++ Trefftest/no.eat.bbt/Classes/admin.php
@@ -1,3 +1,9 @@
 <?php
-nice
+date_default_timezone_set ( 'Europe/Oslo' );
+require_once ('libs/Smarty.class.php');
+
+$smarty = new Smarty ();
+$smarty->display ( 'login.tpl' );
+
 ?>
+
--- Trefftest/no.eat.bbt/Classes/index.php
+++ Trefftest/no.eat.bbt/Classes/index.php
@@ -1,1 +1,8 @@
 <?php
+date_default_timezone_set ( 'Europe/Oslo' );
+require_once ('../libs/Smarty.class.php');
+
+$smarty = new Smarty ();
+$smarty->display ( '../html/login.tpl' );
+
+?>
