489660d7262d7ccc3fcba854694c91e37b318c71
eivindjs <eivind.skreddernes@gmail.com>
2014-12-04 14:06:39 +0100

    databasehelper

--- Trefftest/no.eat.bbt/Repository/databasehelper.class.php
+++ Trefftest/no.eat.bbt/Repository/databasehelper.class.php
@@ -1,3 +1,46 @@
 <?php
-HEy
+date_default_timezone_set ( 'Europe/Oslo' );
+class myDB {
+	protected $db;
+	function __construct($strDSN, $strUser, $strPassword) {
+		try {
+			$this->db = new PDO ( $strDSN, $strUser, $strPassword );
+		} catch ( PDOException $e ) {
+			// throw new Exception($e->getMessage(), $e->getCode);
+			printf ( $e->getMessage () );
+		}
+	}
+	function QueryDatabase($str_prepQuery, $arr_placeholders) {
+		$dirty = array ();
+		$stmt = $this->db->prepare ( $str_prepQuery );
+		if (! $stmt->execute ( $arr_placeholders )) {
+			$arr_error = $stmt->errorInfo ();
+			print_r ( $arr_error );
+			throw new Exception ( "Det skjedde en feil under spørring mot databasen.<br /> Feilmelding: " . @$arr_error [2] );
+			return;
+		}
+		$index = 0;
+		while ( $row = $stmt->fetch ( PDO::FETCH_ASSOC ) ) {
+			$dirty [$index] = $row;
+			$index ++;
+		}
+		return $dirty;
+	}
+	function QueryDb($str_Query) {
+		$dirty = array ();
+		try {
+			$stmt = $this->db->query ( $str_Query );
+			while ( $row = $stmt->fetch ( PDO::FETCH_ASSOC ) ) {
+				$dirty [$index] = $row;
+				$index ++;
+			}
+			$dbh = null;
+		} catch ( PDOException $e ) {
+			print "Error!: " . $e->getMessage () . "<br/>";
+			die ();
+		}
+		
+		return $dirty;
+	}
+}
 ?>
