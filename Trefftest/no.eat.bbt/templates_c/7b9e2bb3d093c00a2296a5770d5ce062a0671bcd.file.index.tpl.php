62c8f1d8c273cf5bc1806a6f67955c832f42b513
eivindjs <eivind.skreddernes@gmail.com>
2014-12-05 12:08:36 +0100

    jau

---
+++ Trefftest/no.eat.bbt/templates_c/7b9e2bb3d093c00a2296a5770d5ce062a0671bcd.file.index.tpl.php
@@ -0,0 +1,26 @@
+<?php /* Smarty version Smarty-3.1.21-dev, created on 2014-12-05 11:29:54
+         compiled from "html\index.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:2028154818922b09f49-96622080%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '7b9e2bb3d093c00a2296a5770d5ce062a0671bcd' => 
+    array (
+      0 => 'html\\index.tpl',
+      1 => 1417775348,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '2028154818922b09f49-96622080',
+  'function' => 
+  array (
+  ),
+  'has_nocache_code' => false,
+  'version' => 'Smarty-3.1.21-dev',
+  'unifunc' => 'content_54818922bc5764_22361677',
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_54818922bc5764_22361677')) {function content_54818922bc5764_22361677($_smarty_tpl) {?><html>
+<p>hei! velkommen til index</p>
+<hr/>
+<a href="Login.php">Logg inn</a>
+</html><?php }} ?>
