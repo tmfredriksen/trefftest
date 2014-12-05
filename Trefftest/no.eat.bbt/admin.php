6502b0d729f04e96b0ad16b883601535055dd1c5
Andreas D Jansson <drknert@gmail.com>
2014-12-05 14:03:17 +0100

    assd

--- Trefftest/no.eat.bbt/admin.php
+++ Trefftest/no.eat.bbt/admin.php
@@ -1,14 +1,25 @@
 <?php
 
+include_once ('Classes/Bruker.class.php');
+include_once ('libs/Smarty.class.php');
+include_once ('init.php');
+include_once ('Repository/databasehelper.class.php');
+include_once ('Repository/HashHelper.php');
+
+$smarty = new Smarty;
+
 if(isset($_SESSION['loggedIn']) && $_SESSION['loggedIn'])
 {
 	if(isset($_SESSION["user"]) && $_SESSION['user']->verify()) {
 		if($_SESSION['isAdmin']) 
 		{
 			
+			$databasehelper = new databasehelper();
 			
+			$treffliste = $databasehelper->getTreffForGodkjenning();
 			
-			
+			$smarty->assign('treffliste', $treffliste);
+		
 		}
 		else {
 			
