7759334cf40a03c27bcaa94d350454939a4f8ba4
Andreas D Jansson <drknert@gmail.com>
2014-12-18 19:09:52 +0100

    jau

---
+++ Trefftest/no.eat.bbt/templates_c/efca128d29e45b12068131a242d4aa99768dbc60.file.visRegion.tpl.php
@@ -0,0 +1,31 @@
+<?php /* Smarty version Smarty-3.1.21-dev, created on 2014-12-18 19:08:25
+         compiled from "html\visRegion.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:213054931819327017-07946377%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    'efca128d29e45b12068131a242d4aa99768dbc60' => 
+    array (
+      0 => 'html\\visRegion.tpl',
+      1 => 1418925918,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '213054931819327017-07946377',
+  'function' => 
+  array (
+  ),
+  'has_nocache_code' => false,
+  'version' => 'Smarty-3.1.21-dev',
+  'unifunc' => 'content_549318193907a6_57745244',
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_549318193907a6_57745244')) {function content_549318193907a6_57745244($_smarty_tpl) {?><?php echo $_smarty_tpl->getSubTemplate ('html/header.tpl', $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, null, array(), 0);?>
+
+
+<html>
+<br/>
+<h2>Region Nord</h2>
+</html>
+
+<?php echo $_smarty_tpl->getSubTemplate ('html/footer.tpl', $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, null, array(), 0);?>
+<?php }} ?>
